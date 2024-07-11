03f8a470943322eac88a40a464d4a0f1b7b42744
DFEFELOV01 <dima.fefelov97.fefelov@mail.ru>
2024-07-11 16:28:45 +0500

    complete creating homework3

--- homework_03/app.py
+++ homework_03/app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
-app=FastAPI()
+app = FastAPI()
+
+
+@app.get("/")
+def index_page():
+    return {"This is ": "homework3!"}
+
 
 @app.get("/ping/")
-def pingapp():
+def ping_page():
     return {"message": "pong"}
